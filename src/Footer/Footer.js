ec5c577b91613ce6fee2ae7fc38ad3ab5d899bc6
Helen Nguyen <hnguye89@uncc.edu>
2023-10-02 01:39:16 -0400

    Adding SASS React

---
+++ src/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+        <p> All rights reserved &copy; Helen Nguyen</p>
+    </footer>
+  );
+}
+
+export default Footer; 
---
+++ src/Menu/Menu.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import {
+    Link
+  } from 'react-router-dom';
+
+function Menu() {
+  return (
+    <nav class="menu">
+        <ul>
+            <li><Link to="/">Homepage</Link></li>
+            <li><Link to="/about.html">About</Link></li>
+            <li><Link to="/login.html">Login</Link></li>
+            <li><Link to="https://google.com">Google</Link></li>
+        </ul>
+    </nav>  
+  );
+}
+
+export default Menu; 
